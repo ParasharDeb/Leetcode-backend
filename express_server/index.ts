2924bdff8197be4ee9f750a69548cf8e8d7fb41f
ParasharDeb <parashardeb11@gmail.com>
2026-08-16 16:51:26 +0530

    added prisma

--- express_server/index.ts
+++ express_server/index.ts
@@ -1,1 +1,18 @@
-console.log("Hello via Bun!");
+import express from "express"
+import cors from "cors"
+import {prisma} from "@repo/database"
+const app=express();
+app.use(cors())
+app.get("/get-questions",async(req,res)=>{
+    const userID=req.body.userId
+    const data =  await prisma.user.findFirst({
+        where:{
+            id:userID
+        }
+    })
+})
+app.get("/:userId/questions",(req,res)=>{
+    // all solved questions of the user
+})
+
+app.listen(8080);
